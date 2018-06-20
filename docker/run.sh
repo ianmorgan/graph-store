34a7a92d91a7f9ae9bbe781da5a2883a441ba152
Ian Morgan <ian.j.morgan@gmail.com>
2018-06-20 23:36:48 +0100

    trying to fix corrupt run.sh file

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,4 @@
-java -Xmx64m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGC -cp /home/app/doc-store-app.jar:/home/app/doc-store-deps.jar ianmorgan.docstore.AppKt
+#!/bin/bash
 
-#java -cp /home/app/doc-store-app.jar:/home/app/doc-store-deps.jar ianmorgan.docstore.AppKt
+exec java -Xmx64m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGC -cp /home/app/doc-store-app.jar:/home/app/doc-store-deps.jar ianmorgan.docstore.AppKt
+
