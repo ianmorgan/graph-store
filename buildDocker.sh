3bce06ce6aa21ce3f836cac92298c8e9a3e3a114
Ian Morgan <ian.j.morgan@gmail.com>
2018-06-24 09:11:48 +0100

    adding docker deploy phase to travis

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 docker build -t graph-store .
 docker tag graph-store:latest ianmorgan/graph-store:latest
 docker push ianmorgan/graph-store:latest
