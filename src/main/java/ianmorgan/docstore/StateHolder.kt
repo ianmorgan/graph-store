020e9fa1459e3f0a95912b920fe00d306cd7f159
Ian Morgan <ian.j.morgan@gmail.com>
2018-06-25 09:51:23 +0100

    added spec for StateHolder class

--- src/main/java/ianmorgan/docstore/StateHolder.kt
+++ src/main/java/ianmorgan/docstore/StateHolder.kt
@@ -4,6 +4,8 @@ import graphql.GraphQL
 import ianmorgan.docstore.dal.DocsDao
 import ianmorgan.docstore.dal.EventStoreClient
 import ianmorgan.docstore.graphql.GraphQLFactory2
+import java.io.File
+import java.io.FileInputStream
 
 /**
  * Singleton to pass around that has all application state.
@@ -13,24 +15,59 @@ class StateHolder (eventStoreClient: EventStoreClient) {
 
     lateinit var docsDao: DocsDao
     lateinit var graphQL: GraphQL
+    var valid = false
+    var exception: RuntimeException? = null
 
     /**
      * Rebuild using the provided schema
      */
-    fun build(graphQLSchema: String) {
-        val dao = DocsDao(graphQLSchema, eventStoreClient)
-        docsDao = dao
+    fun build(graphQLSchema: String) : Boolean {
+        try {
+            val dao = DocsDao(graphQLSchema, eventStoreClient)
+            docsDao = dao
 
-        val ql = GraphQLFactory2.build(graphQLSchema, dao)
-        graphQL = ql
+            val ql = GraphQLFactory2.build(graphQLSchema, dao)
+            graphQL = ql
+
+            valid = true
+            exception = null
+            return true
+        }
+        catch (ex :RuntimeException){
+            exception = ex
+            valid = false
+            return false
+        }
+    }
+
+    fun build(schemaFile: File) : Boolean {
+        val schemaString = FileInputStream(schemaFile).bufferedReader().use { it.readText() }
+        return build(schemaString)
     }
 
     fun docsDao(): DocsDao {
+        if (!isValid()) throw RuntimeException("DocsDao unavailable")
         return docsDao
     }
 
     fun graphQL(): GraphQL {
+        if (!isValid()) throw RuntimeException("DocsDao unavailable")
         return graphQL
+    }
+
+    /**
+     * Is the stateholder valid, i.e. has everything been initialised ok
+     */
+    fun isValid () : Boolean{
+        return valid
+    }
+
+
+    /**
+     * If there has been a problem, expose it to the outside world
+     */
+    fun exception () : RuntimeException? {
+        return exception
     }
 
 
---
+++ src/test/java/ianmorgan/docstore/StateHolderSpec.kt
@@ -0,0 +1,40 @@
+package ianmorgan.docstore
+
+import com.natpryce.hamkrest.*
+import com.natpryce.hamkrest.assertion.assert
+import graphql.schema.idl.errors.SchemaProblem
+import ianmorgan.docstore.dal.DocsDao
+import ianmorgan.docstore.dal.InMemoryEventStore
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import java.io.File
+
+@RunWith(JUnitPlatform::class)
+object StateHolderSpec : Spek({
+
+    val starWarSchema = File("src/schema/starwars.graphqls")
+
+    val stateHolder = StateHolder(InMemoryEventStore())
+
+    describe ("the StateHolder singleton ") {
+
+        it ("should build with a valid schema") {
+            val result = stateHolder.build(starWarSchema)
+            assert.that(stateHolder.isValid() , equalTo(true))
+            assert.that(result, equalTo(true))
+            assert.that(stateHolder.exception() , absent())
+            assert.that(stateHolder.docsDao().availableDocs() , equalTo(setOf("Human","Droid")))
+        }
+
+        it ("should have 'invalid' state if a bad schema") {
+            val result = stateHolder.build("a rubbish schema")
+            assert.that(stateHolder.isValid() , equalTo(false))
+            assert.that(result, equalTo(false))
+            assert.that(stateHolder.exception(), present())
+            assert.that(stateHolder.exception()!!.message, equalTo("errors=[InvalidSyntaxError{ message=Invalid Syntax ,locations=[SourceLocation{line=1, column=0}]}]"))
+        }
+    }
+})
