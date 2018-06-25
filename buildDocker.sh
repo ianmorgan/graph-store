8beb6600a606f137210a44fd4608b752d9b1c42f
Ian Morgan <ian.j.morgan@gmail.com>
2018-06-25 08:21:21 +0100

    updated build to tag docker images with the name travis

--- buildDocker.sh
+++ buildDocker.sh
@@ -5,5 +5,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build -t graph-store .
-docker tag graph-store:latest ianmorgan/graph-store:latest
-docker push ianmorgan/graph-store:latest
+docker tag graph-store:latest ianmorgan/graph-store:travis
+docker push ianmorgan/graph-store:travis
