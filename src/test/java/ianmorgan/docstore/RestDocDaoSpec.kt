48d36a30a2246ba13c62ecd1ce515e098b740f9b
Ian Morgan <ian.j.morgan@gmail.com>
2018-07-25 11:12:30 +0100

    adding support for configurable groovy script for mapping of REST query

--- src/test/java/ianmorgan/docstore/RestDocDaoSpec.kt
+++ src/test/java/ianmorgan/docstore/RestDocDaoSpec.kt
@@ -4,6 +4,7 @@ import com.natpryce.hamkrest.assertion.assert
 
 import com.natpryce.hamkrest.equalTo
 import graphql.schema.idl.SchemaParser
+import ianmorgan.docstore.dal.ConfigurableRestDocDao
 import ianmorgan.docstore.dal.StarshipDocDao
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
@@ -14,16 +15,15 @@ import org.junit.runner.RunWith
 @RunWith(JUnitPlatform::class)
 object RestDocDaoSpec : Spek({
 
-
     describe("A REST DAO for external starwars data") {
         beforeGroup {
             val schemaParser = SchemaParser()
         }
 
         it("should use the 'ID' field as the aggregate id") {
-            val dao = StarshipDocDao()
+            val dao = ConfigurableRestDocDao()
 
-            val result = dao.retrieve("Millennium Falcon")
+            val result = dao.retrieve("10")
             val expected = mapOf ("name" to "Millennium Falcon",
                 "manufacturer" to "Corellian Engineering Corporation",
                 "model" to "YT-1300 light freighter",
@@ -33,12 +33,7 @@ object RestDocDaoSpec : Spek({
 
            assert.that(result, equalTo(expected))
         }
-
-
     }
-
-
-
 
 
 })
