b7b5fa300998b9db7805a25e5ce19593c5997dba
Ian Morgan <ian.j.morgan@gmail.com>
2018-08-21 13:38:31 +0100

    working on argswalker

--- src/main/java/ianmorgan/graphstore/graphql/ArgsWalker.kt
+++ src/main/java/ianmorgan/graphstore/graphql/ArgsWalker.kt
@@ -3,24 +3,28 @@ package ianmorgan.graphstore.graphql;
 
 /**
  * Navigates the query args. Basically this is a map that represents
- * the underlying gradually query, and as we recurse through the graph
+ * the underlying GraphQL query, and as we recurse through the graph
  * we need to keep track of which part of the tree matters. Slightly
- * similar to walking a directory tree
+ * similar to walking a directory tree.
+ *
+ * The order of the original map is retained through use of a LinkedHashMap
  *
  * @constructor Pass the current path or "/" string for root and the args under that node
  */
-class ArgsWalker constructor(path : String, args : Map<String,Map<String,Any>>) {
-    val path = path
+class ArgsWalker constructor(path : String, args : Map<String,Map<String,Any>>, parent : ArgsWalker? = null) {
+    val path = if (path.endsWith("/")) path else "$path/"
     val args = args
+    val parent = parent
 
 
     /**
-     * The current path, i.e. the node in the graph
+     * The current path, i.e. the node in the graph. The trailing slash is
+     * removed unless this the root, e.g. root will be "/", whereas the a child
+     * node would be "friends"
      */
     fun path() : String {
-        return path
+        return if (path == "/") path else path.substring(0,path.length-1)
     }
-
 
     /**
      * The current set of fields (args) under the node
@@ -30,7 +34,17 @@ class ArgsWalker constructor(path : String, args : Map<String,Map<String,Any>>) 
     }
 
     /**
+     * The node above, or null if this is the top of the tree
+     */
+    fun parent() : ArgsWalker? {
+        return parent
+    }
+
+    /**
      * Walk down into the node
+     *
+     * @param path - The path, can be with or without the trailing slash (same basic
+     *               rules as directory listing in bash), e.g. "friends" or "friends/"
      */
     fun walkPath(path : String) : ArgsWalker {
         val standardPath = if (path.endsWith("/")) path else "$path/"
@@ -43,7 +57,7 @@ class ArgsWalker constructor(path : String, args : Map<String,Map<String,Any>>) 
             }
         }
 
-        return ArgsWalker(path, working)
+        return ArgsWalker(standardPath, working, this)
     }
 
 }
--- src/test/java/ianmorgan/graphstore/ArgsWalkerSpec.kt
+++ src/test/java/ianmorgan/graphstore/ArgsWalkerSpec.kt
@@ -2,42 +2,44 @@ package ianmorgan.graphstore
 
 import com.natpryce.hamkrest.assertion.assert
 import com.natpryce.hamkrest.equalTo
-import ianmorgan.github.io.jsonUtils.JsonHelper
 import ianmorgan.graphstore.graphql.ArgsWalker
-import io.javalin.Javalin
-import org.apache.commons.cli.DefaultParser
-import org.apache.commons.cli.Options
-import org.eclipse.jetty.util.URIUtil
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.json.JSONObject
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 
 @RunWith(JUnitPlatform::class)
 object ArgsWalkerSpec : Spek({
 
-    lateinit var app: Javalin
-    val baseUrl = "http://localhost:8002/"
+    val example = mapOf(
+        "name" to emptyMap<String, Any>(),
+        "friends" to mapOf("length" to 10),
+        "friends/name" to emptyMap()
+    )
 
     describe("walking the query args") {
 
-            beforeEachTest {}
+        it("should walk simple args") {
 
-            it("should walk simple args") {
-                val args = mapOf("name" to emptyMap<String,Any>(),
-                    "friends" to emptyMap(),
-                    "friends/name" to emptyMap())
+            val root = ArgsWalker("/", example)
+            val friends = root.walkPath("friends")
 
-                val root = ArgsWalker("/", args)
-
-                val friends = root.walkPath("friends")
-                val expected = mapOf("name" to emptyMap<String,Any>())
-
-                assert.that(friends.args, equalTo(expected))
-            }
+            assert.that(friends.args(), equalTo(mapOf("name" to emptyMap<String, Any>())))
+            assert.that(friends.path(), equalTo("friends"))
+            assert.that(friends.parent(), equalTo(root))
         }
+
+        it("should build root node") {
+            val root = ArgsWalker("/", example)
+
+            assert.that(root.args(), equalTo(example))
+            assert.that(root.path(), equalTo("/"))
+
+            // how to do a null check using hamcrest
+            //assert.that(root.parent(), nothing)
+        }
+
+    }
 
 })
